14fa72139587ff2495a9fd1cf327d193624f00eb
buildwithujjwal <ujjwal.work04@gmail.com>
2026-08-07 19:52:05 +0530

    feat: add skeleton structure for all Flowdesk pages.

--- js/nav.js
+++ js/nav.js
@@ -2,4 +2,11 @@ fetch('../partials/nav.html')
     .then(response => response.text())
     .then(html => {
         document.getElementById("nav-placeholder").innerHTML = html;
-    })
+
+        const logoutBtn = document.getElementById("logout-btn");
+        logoutBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        window.location.href = "index.html";
+            });
+    });
+
